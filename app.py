63868e1e0b8e774784045780d3357b5c76f6f380
MrSirene <ffasgaming@alienplant.com>
2021-01-13 17:11:43 +0200

    Added some basic stuff

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask, url_for, request, redirect, render_template
 from flask_login import FlaskLoginClient
 
 app = Flask(__name__)
@@ -9,8 +9,26 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/news')
+def show_news():
+    return render_template('news.html')
+
+
+@app.route('/contact')
+def show_contacts():
+    return render_template('contact.html')
+
+
+@app.route('/about')
+def show_about():
+    return render_template('about.html')
+
+
 @app.route('/login')
 def login():
     return 'login'
-
-
