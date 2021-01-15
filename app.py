6334d13799db1f942ed489af2a0b77c14b9dc33f
MrSirene <ffasgaming@alienplant.com>
2021-01-15 14:18:04 +0200

    login/register css

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, \
     logout_user, current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField,
 from wtforms.validators import InputRequired, Length
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_bootstrap import Bootstrap
