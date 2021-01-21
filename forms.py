d31c15047033a2b6cf7c8f181c23582304e8d7f6
MrSirene <ffasgaming@alienplant.com>
2021-01-21 18:25:22 +0200

    Front-end css/html , moved flash messages below navbar

--- forms.py
+++ forms.py
@@ -6,25 +6,25 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 class RegistrationForm(FlaskForm):
     username = StringField('username',
-                           validators=[InputRequired(), Length(min=3, max=20)])
+                           validators=[InputRequired(), Length(min=3, max=20)], render_kw={"placeholder": "Username"})
     email = StringField('email',
-                        validators=[InputRequired(), Length(max=50)])
+                        validators=[InputRequired(), Length(max=50)], render_kw={"placeholder": "E-mail"})
     password = PasswordField('password',
-                             validators=[InputRequired(), Length(min=8)])
+                             validators=[InputRequired(), Length(min=8)], render_kw={"placeholder": "Password"})
 
 
 class LoginForm(FlaskForm):
     username = StringField('username',
-                           validators=[InputRequired()])
+                           validators=[InputRequired()], render_kw={"placeholder": "Username"})
     password = PasswordField('password',
-                             validators=[InputRequired(), Length(min=8)])
+                             validators=[InputRequired(), Length(min=8)], render_kw={"placeholder": "Password"})
     remember = BooleanField('remember me')
 
 
 class ApartmentForm(FlaskForm):
     name = TextAreaField('name',
-                         validators=[InputRequired(), Length(min=3, max=255)])
+                         validators=[InputRequired(), Length(min=3, max=255)], render_kw={"placeholder": "Name for your apartment. (3-255 symbols)"})
     location = TextAreaField('location',
-                             validators=[InputRequired(), Length(min=10)])
+                             validators=[InputRequired(), Length(min=10)], render_kw={"placeholder": "Where is it located ? (Min. 10 symbols)"})
     description = TextAreaField('description',
-                                validators=[InputRequired(), Length(min=10)])
+                                validators=[InputRequired(), Length(min=10)], render_kw={"placeholder": "A description of your apartment. (Min. 10 symbols)"})
